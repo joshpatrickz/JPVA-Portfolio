3e3d383eea40939b1161de03a2929f77e833262a
Joshua Patrick Velches Aquino <patrickz0985@gmail.com>
2023-05-29 12:51:30 +0800

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,44 +1,44 @@
-import JPVAPortfolio from '../assets/portfolio-screenshot.png'
-import ProjectImg from '../assets/ProjectImg.jpg'
-import PortfolioTemplate from "../assets/portfoliotemplate.png"
+import JPVAPortfolio from "../assets/portfolio-screenshot.png";
+import ProjectImg from "../assets/ProjectImg.jpg";
+import PortfolioTemplate from "../assets/portfoliotemplate.png";
 
 const projectData = [
-    {
-      title: "JPVA Portfolio",
-      imgUrl: `${JPVAPortfolio}`,
-      stack: ["React", "Tailwind CSS", "JavaScript"],
-      link: "https://github.com/joshpatrickz/jpva.github.io/",
-    },
-    {
-      title: "Website Portfolio Template for sale",
-      imgUrl: `${PortfolioTemplate}`,
-      stack: ["HTML", "CSS", "JavaScript"],
-      link: "#",
-    },
-    {
-      title: "Project 3",
-      imgUrl: `${ProjectImg}`,
-      stack: ["HTML", "CSS", "JavaScript"],
-      link: "#",
-    },
-    {
-      title: "Project 4",
-      imgUrl: `${ProjectImg}`,
-      stack: ["HTML", "CSS", "JavaScript"],
-      link: "#",
-    },
-    {
-      title: "Project 5",
-      imgUrl: `${ProjectImg}`,
-      stack: ["HTML", "CSS", "JavaScript"],
-      link: "#",
-    },
-    {
-      title: "Project 6",
-      imgUrl: `${ProjectImg}`,
-      stack: ["HTML", "CSS", "JavaScript"],
-      link: "#",
-    },
-  ];
-  
-  export default projectData;
+  {
+    title: "JPVA Portfolio",
+    imgUrl: `${JPVAPortfolio}`,
+    stack: ["React", "Tailwind CSS", "JavaScript"],
+    link: "https://github.com/joshpatrickz/jpva.github.io/",
+  },
+  {
+    title: "Website Portfolio Template for sale",
+    imgUrl: `${PortfolioTemplate}`,
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://jpva-portfolio-template.netlify.app/",
+  },
+  {
+    title: "Project 3",
+    imgUrl: `${ProjectImg}`,
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "#",
+  },
+  {
+    title: "Project 4",
+    imgUrl: `${ProjectImg}`,
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "#",
+  },
+  {
+    title: "Project 5",
+    imgUrl: `${ProjectImg}`,
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "#",
+  },
+  {
+    title: "Project 6",
+    imgUrl: `${ProjectImg}`,
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "#",
+  },
+];
+
+export default projectData;
