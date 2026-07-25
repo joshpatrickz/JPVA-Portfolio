fd9535da3cf8c6d75303467927557503d51c3190
Joshua Patrick Velches Aquino <patrickz0985@gmail.com>
2026-07-25 09:42:51 +0800

    Update_about_20260725

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
     <div
       name="about"
       id="About"
-      className="w-full h-[500px] flex flex-col bg-[#E1C340] shadow-xl"
+      className="w-full h-[300px] flex flex-col bg-[#E1C340] shadow-xl"
     >
-      <div className="md:max-w-[1000px] sm:max-w-[600px] max-w-[400px] md:mt-20 sm:mt-14 mt-10 mb-10 mx-auto text-center justify-center">
+      <div className="md:max-w-[800px] sm:max-w-[600px] max-w-[400px] md:mt-20 sm:mt-14 mt-10 mb-10 mx-auto text-center justify-center">
         <h1 className="md:text-4xl sm:text-2xl text-xl font-bold text-white py-4">
           Hi. I'm Josh. Nice to meet you!
         </h1>
